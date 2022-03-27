e7c031fe9fb83ebe60a88c9ab5e53f2eda5019c7
YasminJawadha <jawadha555@gmail.com>
2022-03-28 00:31:27 +0530

    UPDAT

--- login.js
+++ login.js
@@ -3,46 +3,62 @@ let email = document.getElementById("form2Example18");
 let pwd = document.getElementById("form2Example28");
 let error = document.getElementById("error");
 
-
-// form.addEventListener('submit', (e)=>{
-//     e.preventDefault();
-//     checkInputs();
-// })
-function submitData(){  nm 
-        document.getElementById("form1").submit();
-    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if(email.value.match(mailformat))
-    {
-    return true;
-    }
-    else
-    {
-        error.innerHTML ="You have entered an invalid email address!";
-        error.style.color = "red";
-        return false;
-    }
     
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+
+    checkInputs();
+});
+
+function checkInputs() {
+    //get the values of the input
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+
+    if (emailValue ===''){
+        //show error
+        //add error class
+        setErrorfor(email, "email cannot be blank");
+    }
+    else if(!isEmail(emailValue)){
+        setErrorfor(email, "email is incorrect");
+    }
+    else{
+        //add success class
+        setSuccessfor(email);
+    }
+    if (passwordValue ===''){
+        //show error
+        //add error class
+        setErrorfor(password, "password cannot be blank");
+    }
+    else{
+        //add success class
+        setSuccessfor(password);
+    }
+
+}  
+
+
+function setErrorfor(input, message){
+    const formControl = input.parentElement;//.form-control
+    const small = formControl.querySelector('small');
+
+    //add error message inside small tag
+    small.innerText = message;
+
+    //add error class
+    formControl.className= 'form-control error';
+
 }
 
-function checkInputs(){
-
-    // const email = email.value.trim();
-    // const pwd = pwd.value.trim();
+function setSuccessfor(input){
+    const formControl = input.parentElement;//.form-control
     
+    formControl.className= 'form-control success';
 
+}
 
-    
-    // let regexp = /^([A-Za-z0-9\.-]+)@([A-Za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3}?)$/
-    // if(regexp.test(email.value)){
-    //     error.innerHTML = "valid";
-    //     error.style.color= "green";
-    //     return true;
-    // }
-    // else{
-    //     error.innerHTML ="invalid";
-    //     error.style.color = "red";
-    //     return false;
-    // }
-
-
+function isEmail(email){
+    return /^([A-Za-z0-9\.-]+)@([A-Za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3}?)$/.test(email);
 }
